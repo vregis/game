39686697c0f6b6d2d621596c78ae5c5da8d0bbc2
stark <vregis@mail.ru>
2026-08-25 19:59:00 +0300

    автозапуск тура

--- frontend/views/quest/new-tour.php
+++ frontend/views/quest/new-tour.php
@@ -1,18 +1,18 @@
 Добро пожаловать в тур номер '<?php echo $tour->number?>'</p>
 <p><?php echo $tour->name?></p>
-<button data-id = <?php echo $tour->id?> data-url="<?php echo \yii\helpers\Url::to(['/quest/tour-start'])?>" class="btn btn-success tour-start">
+<button id="start-tour" data-id = <?php echo $tour->id?> data-url="<?php echo \yii\helpers\Url::to(['/quest/tour-start'])?>" class="btn btn-success tour-start">
     Начать Тур
 </button>
 
 <?php
 $this->registerJs("
+
     
-    $('.tour-start').click(function(){
         
         $.ajax({
                 type: 'POST',
-                data:{id: $(this).attr('data-id')},
-                url: $(this).attr('data-url'),
+                data:{id: $('.tour-start').attr('data-id')},
+                url: $('.tour-start').attr('data-url'),
                 dataType: 'json',
                 success: function(msg){
                     if (msg.success == true ) {
@@ -20,7 +20,7 @@ $this->registerJs("
                     }
                 }
             })
-    })
+
     
 ")
 ?>
